9f11e61cdf634714d16261ca56343d9afa2edf73
Ake Hedman <akhe@grodansparadis.com>
2024-10-15 21:26:30 +0200

    Added MAX6675 smoke temeprature sensor demo

---
+++ samples/python/smoke_gas_temp.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+// File: send_smoke_temp.py
+//
+// Usage: send_smoke_temp host user password [guid]
+//
+// Described here https://github.com/grodansparadis/vscp-samples/tree/master/samples/python
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version
+// 2 of the License, or (at your option) any later version.
+//
+// This file is part of the VSCP (http://www.vscp.org)
+//
+// Copyright (C) 2000-2024
+// Ake Hedman, Grodans Paradis AB, <akhe@grodansparadis.com>
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+//
+"""
+
+import subprocess
+import getpass
+import telnetlib
+import sys
+
+# get smoke temperature
+result = subprocess.run(['./readmax6675'], stdout=subprocess.PIPE)
+smoketemp = result.stdout
+print(smoketemp)
+
+if ( len(sys.argv) < 4 ):
+	sys.exit("Wrong number of parameters - aborting")
+
+guid = "-"
+host = sys.argv[1]
+user = sys.argv[2]
+password = sys.argv[3]
+if ( len(sys.argv) > 3 ):
+    guid = sys.argv[4]
+
+
+# Connect to VSCP daemon
+tn = telnetlib.Telnet(host, 9598)
+tn.read_until(b"+OK",2)
+
+# Login
+tn.write(b"user " + user.encode("ascii") + b"\n")
+tn.read_until(b"+OK", 2)
+
+tn.write(b"pass " + password.encode("ascii") + b"\n")
+tn.read_until(b"+OK - Success.",2)
+
+event = "3,"		# Priority=normal
+event += "10,6,"	# Temperature
+event += ","		# DateTime set to current by VSCP daemon
+event += "0,"		# Use interface timestamp
+event += "0,"  		# Use obid of interface
+event += guid  + ","	# add GUID to event
+#event += "0,255,255"    # To all zones/subzones
+
+# datacoding = String format| Celsius | sensor 0
+datacoding = 0x40 | (1<<3) | 0  
+event += hex(datacoding)	# Add datacoding byte to event
+
+# Make sure length is OK (max seven characters)
+tempstr = smoketemp.decode('ascii');
+tempstr = tempstr.strip()
+if ( len(tempstr) > 7 ):
+    tempstr = tempstr[0:7]
+print(tempstr)    
+
+# Write temperature into the event (not line breaks)
+for ch in tempstr:
+    event += ","
+    event += hex(ord(ch))
+
+
+# Send event to server
+tn.write(b"send " + event.encode("ascii") + b"\n")
+tn.read_until(b"+OK - Success.",2)
+
+tn.write(b"quit\n")
+
